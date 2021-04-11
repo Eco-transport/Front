c1e5d56a8b4aad3e1d669d987f1c2861c7a21b6e
Klalvarezm <klalvarezm@unal.edu.co>
2021-04-11 10:12:37 -0500

    pequeños cambios

--- Ecotransport/src/views/CuentaEliminada.vue
+++ Ecotransport/src/views/CuentaEliminada.vue
@@ -3,7 +3,7 @@
         <Header></Header>
         <h3>Tu cuenta ha sido eliminada exitosamente</h3>
         <p>Deseamos conocer la razon de tu partida, por lo cual si deseas, puedes contactarnos a traves de la pagina
-            o en nuestro correo EcoTransport@gmail.com , te atenderemos con todo el gusto.
+            o en nuestro correo ecotransport342@gmail.com , te atenderemos con todo el gusto.
         </p>
         <p>Pronto serás redirijido a nuestro menu principal, si esto no ocurre haz click <a href="/#/">aqui</a></p>
         </div>
--- Ecotransport/src/views/Terminos.vue
+++ Ecotransport/src/views/Terminos.vue
@@ -2,6 +2,51 @@
     <div>
         <Header></Header>
         <h1>Terminos y condiciones</h1>
+        <p><strong>1.Terminos Generales</strong> 
+        La agencia de turismo Biking Bogotá identificada con Nit 80110891-9 y Registro Nacional de Turismo 42640 
+        garantiza la prestación de un servicio de calidad y asesoría continua a sus clientes. 
+        Todos los servicios incluyen asesoría remota gratuita antes y durante la prestación del servicio. 
+        El canal de comunicación oficial entre el cliente y EcoTransport Bogotá será el correo electrónico
+        ecotransport342@gmail.com y el número de WhatsApp +57 313 4989958. La prestación de los servicios dará 
+        inicio una vez el cliente manifieste de manera verbal, escrita o tácita la aceptación de estos términos y
+        condiciones bajo los precios informados a través de los canales de información oficiales.</p>
+        <br>
+        <p><strong>2.Términos y condiciones específicas de la prestación del servicio de alquiler:</strong>
+        <br>
+        <b>2.1 Del alquiler de bicicletas:</b>
+        Se trata del alquiler o préstamo de las bicicletas solicitadas por el cliente, donde se garantiza
+         su funcionamiento en el momento de entrega, de acuerdo con el plan contratado.
+          Cada bicicleta tiene un precio de alquiler dependiendo de las especificaciones mecánicas
+           de la bicicleta y el plan de alquiler escogido. 
+        El lugar de entrega y devolución será en las oficinas de
+        EcoTransport Bogotá ubicada en los sectores establecidos al momento de pedir el prestamo.  
+        El horario de atención para entrega y devolución es tambien, el acordado al momento de la solicitud.
+        El cliente por voluntad propia acepta incondicionalmente acatar y respetar las normas de tránsito
+         y demás legislación colombiana aplicable.
+         <br>
+         <b>2.2 Del tiempo de alquiler:</b>
+         Los precios publicados en la página web corresponden al uso o disfrute de la bicicleta
+          y sus accesorios de acuerdo al plan escogido por el cliente, 
+           dentro del horario estipulado por EcoTransport Bogotá para su uso, esto quiere 
+           decir únicamente los horarios acordados. Los planes de alquiler no podrán ser divididos, 
+           repartidos o fraccionados en varias franjas horarias el mismo día o en varios días.
+        <br>
+        <b>2.3 De la entrega y devolución de la(s) bicicletas y los accesorios:</b>
+        Cuando el cliente tiene una reserva previamente confirmada de acuerdo a las condiciones establecidas
+         por la agencia, la(s) bicicleta(s) se entregan en nuestra oficina principal en el horario
+          previamente acordado con el cliente. Por tal razón agradecemos tener en cuenta que 
+          el servicio de alquiler comienza a la hora previamente acordada, 
+          independientemente si el cliente ha llegado o no a la oficina para la entrega del equipo a alquilar.
+        Por lo anterior, sugerimos llegar a la oficina de EcoTransport Bogotá con al menos 10 minutos
+         de anticipación para iniciar su servicio a tiempo, ya que se hará una revisión
+          previa del equipo entregado, se firmará el contrato de alquiler y se recibirá el 
+          depósito mencionado en la descripción del producto. 
+        Finalmente, recomendamos revisar bien el mismo antes de salir de la oficina y 
+        cualquier novedad que considere pertinente dejarla por escrito en el contrato de alquiler; 
+        ya que, de lo contrario se presume que el cliente recibió la bicicleta y el equipo objeto de
+         alquiler en buenas condiciones para su adecuado funcionamiento, sin lugar a ninguna reclamación
+          posterior. (Artículo 1992 del código civil)
+        </p>
     </div>
 </template>
 
