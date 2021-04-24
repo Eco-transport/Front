87ea6f3a784fff3501e016e203bded080fcfb111
Nicolas Rodriguez Salguero <nirodriguezsa@unal.edu.co>
2021-04-24 18:14:19 -0500

    Modificación Header No Log, Implementacion del Login, Pestaña de Editar Crud y Agregar funcionando en Beta con Api Externa

---
+++ Ecotransport/src/components/UnlogHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="Header">
+    <!--Header-->
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #295135;">
+      <a class="navbar-brand" href="/">
+        <img src="@/assets/Logo.png" alt="Logo Ecotransport" height="40" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-md-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="test">Iniciar sesión</a>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    nombre: String,
+  },
+};
+</script>
